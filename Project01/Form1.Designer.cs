0ad81f588f85af797888d24469faafd927e377a9
JonathanSpalding <Jon.Spalding@hotmail.com>
2016-04-30 10:06:19 -0600

    CS 1400 C#

---
+++ Project01/Form1.Designer.cs
@@ -0,0 +1,186 @@
+﻿namespace Project01
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.mealAmountInput = new System.Windows.Forms.TextBox();
+            this.poorServiceOutput = new System.Windows.Forms.TextBox();
+            this.averageServiceOutput = new System.Windows.Forms.TextBox();
+            this.excellentServiceOutput = new System.Windows.Forms.TextBox();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(28, 28);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.exitToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(403, 38);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.exitToolStripMenuItem1,
+            this.aboutToolStripMenuItem});
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(58, 34);
+            this.exitToolStripMenuItem.Text = "Exit";
+            // 
+            // exitToolStripMenuItem1
+            // 
+            this.exitToolStripMenuItem1.Name = "exitToolStripMenuItem1";
+            this.exitToolStripMenuItem1.Size = new System.Drawing.Size(240, 34);
+            this.exitToolStripMenuItem1.Text = "Exit";
+            this.exitToolStripMenuItem1.Click += new System.EventHandler(this.exitToolStripMenuItem1_Click);
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(240, 34);
+            this.aboutToolStripMenuItem.Text = "About";
+            this.aboutToolStripMenuItem.Click += new System.EventHandler(this.aboutToolStripMenuItem_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 57);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(134, 25);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Meal Amount:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 114);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(130, 25);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Poor Service:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 149);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(163, 25);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Average Service:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(13, 183);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(168, 25);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Excellent Service:";
+            // 
+            // mealAmountInput
+            // 
+            this.mealAmountInput.Location = new System.Drawing.Point(200, 53);
+            this.mealAmountInput.Name = "mealAmountInput";
+            this.mealAmountInput.Size = new System.Drawing.Size(163, 29);
+            this.mealAmountInput.TabIndex = 5;
+            this.mealAmountInput.TextChanged += new System.EventHandler(this.mealAmountInput_TextChanged);
+            this.mealAmountInput.Leave += new System.EventHandler(this.mealAmountInput_Leave);
+            // 
+            // poorServiceOutput
+            // 
+            this.poorServiceOutput.Location = new System.Drawing.Point(200, 110);
+            this.poorServiceOutput.Name = "poorServiceOutput";
+            this.poorServiceOutput.Size = new System.Drawing.Size(163, 29);
+            this.poorServiceOutput.TabIndex = 6;
+            // 
+            // averageServiceOutput
+            // 
+            this.averageServiceOutput.Location = new System.Drawing.Point(200, 146);
+            this.averageServiceOutput.Name = "averageServiceOutput";
+            this.averageServiceOutput.Size = new System.Drawing.Size(163, 29);
+            this.averageServiceOutput.TabIndex = 7;
+            // 
+            // excellentServiceOutput
+            // 
+            this.excellentServiceOutput.Location = new System.Drawing.Point(200, 183);
+            this.excellentServiceOutput.Name = "excellentServiceOutput";
+            this.excellentServiceOutput.Size = new System.Drawing.Size(163, 29);
+            this.excellentServiceOutput.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(403, 246);
+            this.Controls.Add(this.excellentServiceOutput);
+            this.Controls.Add(this.averageServiceOutput);
+            this.Controls.Add(this.poorServiceOutput);
+            this.Controls.Add(this.mealAmountInput);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox mealAmountInput;
+        private System.Windows.Forms.TextBox poorServiceOutput;
+        private System.Windows.Forms.TextBox averageServiceOutput;
+        private System.Windows.Forms.TextBox excellentServiceOutput;
+    }
+}
+
---
+++ Project01/Form1.cs
@@ -0,0 +1,67 @@
+﻿// Author: Jonathan Spalding
+// Assignment: Project 01
+// Instructor "Roger deBry
+// Clas: CS 1400
+// Date Written: 1/23/2016
+//
+// "I declare that the following source code was written solely by me. I understand that copying any source code, in whole or in part, constitutes cheating, and that I will receive a zero on this project if I am found in violation of this policy."
+//----------------------------------------------------
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Project01
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        // The exitToolStripMenuItem1_Click Method
+        // Purpose: Display a pop up box when you click About
+        // Parameters: The sending object, and the event arguments
+        // Returns: none
+        private void exitToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+        // The aboutToolStripMenuItem_Clic Method
+        // Purpose: Display a pop up box when you click About
+        // Parameters: The sending object, and the event arguments
+        // Returns: none
+        private void aboutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Jonathan Spalding/nCS1400/Project 1");
+        }
+        // I accidentally clicked on the input text box and forgot to create the "Leave" method. It doesn't let me edit this argument to have _leave or even delete it.
+        private void mealAmountInput_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        // The mealAmountInput_Leave Method
+        // Purpose: take the cost of the meal, and give three outputs for poor, average, and excellent service. It converts the input from a string to a double, then multiplies it by the percentages to get the proper tips, then converts all three outputs into strings, and places it in the proper output boxes.
+        // Parameters: The sending object, and the event arguments
+        // Returns: none
+        private void mealAmountInput_Leave(object sender, EventArgs e)
+        {
+            // I didn't make constants because I figured it's pointless when each constant is only used once. But I understand the benefit of having a constant in a larger program. Also the Interface requirements didn't say I needed constants, just to "See lab #5."
+            double num = double.Parse(mealAmountInput.Text);
+            double poorService = num * .1;
+            double averageService = num * .15;
+            double excellentService = num * .2;
+            string poorServiceOutStr = $"{poorService:c}";
+            string averageServiceOutStr = $"{averageService:c}";
+            string excellentServiceOutStr = $"{excellentService:c}";
+            poorServiceOutput.Text = poorServiceOutStr;
+            averageServiceOutput.Text = averageServiceOutStr;
+            excellentServiceOutput.Text = excellentServiceOutStr;
+        }
+    }
+}
